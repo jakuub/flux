4a048b6ec0778023710884100daf1dfeb8f8d104
Jakuub Uhrik <jakuub@mail.com>
2014-10-21 13:06:43 +0200

    Added insert, update and listen method to store

--- lib/src/store/store.dart
+++ lib/src/store/store.dart
@@ -9,20 +9,56 @@ class Store<P extends Persistent> {
   P get data => _data;
   
   Stream get updated => _updateController.stream;
+  
+  final Dispatcher _dispatcher;
 
-  Store(Dispatcher dispatcher, {P data, StreamController updateController}) : 
+  Store(this._dispatcher, {P data, StreamController updateController}) : 
     _data = data, 
     _updateController = ifNull(updateController, new StreamController());
   
-  P setData(P data) {
+  P setData(P data, {bool notify: true}) {
     P oldData = _data; 
     _data = data;
-    _updateController.add(data != oldData);
+    
+    if (notify) {
+      _updateController.add(data != oldData);
+    }
+    
     return oldData;
   }
   
-  apply(modifier) {
-    setData(modifier(data));
+  apply(modifier, {bool notify: true}) {
+    setData(modifier(data), notify: notify);
+  }
+  
+  P update(List path, Function updater, {bool notify: true}) {
+    return setData(insertIn(data, path, updater(lookupIn(data, path))), notify: notify);
+  }
+  
+  P insert(List path, dynamic value, {bool notify: true}) {
+    return setData(insertIn(data, path, value), notify: notify);
+  }
+  
+  void listen(Map listeners) {
+    listeners.forEach((filter, callback) {
+      if(!(filter is Function)) {
+        filter = _compare(filter);
+      }
+      _dispatcher.stream.where(filter).listen(callback);
+    });
+  }
+  
+  _compare (valueToCompare) {
+    return (value) {
+      if(value is Map && value[TYPE] == valueToCompare) {
+        return true;
+      } else if (value is PersistentMap && value.get(TYPE, null) == valueToCompare) {
+        return true;
+      }
+      return value == valueToCompare;
+    };
   }
 
 }
+
+const String TYPE = "type";
--- test/store/store.dart
+++ test/store/store.dart
@@ -9,20 +9,19 @@ import 'package:mock/mock.dart';
 main() {
   group("(Store)", () {
 
-    Store<Persistent> store;
-    Persistent oldData;
-    Persistent newData;
-    Mock mock;
+    Store<PersistentMap> store;
+    PersistentMap oldData;
+    PersistentMap newData;
+    StreamControllerMock mock;
     StreamController controller;
 
 
     setUp(() {
-      newData = new Persistent();
-      oldData = new Persistent();
+      newData = persistent({"key": {"key2": {"key3": 2}}});
+      oldData = persistent({"key": {"key2": {"key3": 1}}});
       mock = new StreamControllerMock();
       controller = new StreamController();
       store = new Store(null, updateController: mock, data: oldData);
-
     });
 
     test("should have constructor with dispatcher", () {
@@ -102,7 +101,131 @@ main() {
       
       expect(store.data, equals(newData));
     });
+    
+    test("should have method update which update adequate key of passed Persistent object", () {
+      newData = store.update(["key"], (_) => 2);
+      
+      expect(oldData, equals(newData));
+      expect(store.data.get("key"), equals(2));
+      expect(store.data, isNot(equals(newData)));
+    });
+    
+    test("should update deep reference when used update with longer path", () {
+      newData = store.update(["key", "key2", "key3"], (_) => 2);
+      
+      expect(store.data.get("key").get("key2").get("key3"), equals(2));
+    });
+    
+    test("should add event to updated stream when called update", () {
+      store.setData(persistent({"key": 1}));
+      mock.clearLogs();
+      store.update(["key"], (_) => 2);
+      mock.getLogs(callsTo("add")).verify(happenedOnce);
+    });
+    
+    test("should not create updated event if setData have attr notify false", () {
+      store.setData(persistent({1:2}), notify: false);
+      mock.getLogs(callsTo("add")).verify(neverHappened);
+    });
+    
+    test("should not create updated event if apply have attr notify false", () {
+      store.apply((_) => newData, notify: false);
+      
+      mock.getLogs(callsTo("add")).verify(neverHappened);
+    });
+    
+    test("sould not create updated event if update have attr notify false", () {
+      store.update(["key"], (_) => 2, notify: false);
+      
+      mock.getLogs(callsTo("add")).verify(neverHappened);
+    });
+
+    test("should insert deep reference when used insert with longer path", () {
+      newData = store.insert(["key", "key2", "key3"], 2);
+      
+      expect(newData.get("key").get("key2").get("key3"), equals(1));
+      expect(store.data.get("key").get("key2").get("key3"), equals(2));
+    });
+    
+    test("should notify on insert method", () {
+      newData = store.insert(["key", "key2", "key3"], 2);
+      mock.getLogs(callsTo("add")).verify(happenedOnce);
+    });
+
+    test("sould not create updated event if insert have attr notify false", () {
+      store.insert(["key"], 2, notify: false);
+      mock.getLogs(callsTo("add")).verify(neverHappened);
+    });
+    
+    group("(listen)", () {
+      
+      dynamic filter;
+      dynamic callback;
+      StreamMock dispatcherStream;
+      StreamMock filteredDispatcherStream;
+      DispatcherMock dispatcher; 
+      
+      setUp(() {
+        filter =  (ff) => ff == filter;
+        callback = new Mock();
+
+        dispatcherStream = new StreamMock();
+        filteredDispatcherStream = new StreamMock();
+        
+        mock
+          .when(callsTo("get stream"))
+            .alwaysReturn(dispatcherStream);
+        dispatcherStream
+          .when(callsTo("where"))
+            .alwaysReturn(filteredDispatcherStream);
+        
+        dispatcher = new DispatcherMock();
+        
+        dispatcher
+          .when(callsTo("get stream"))
+            .alwaysReturn(dispatcherStream);
+
+        store = new Store(dispatcher, updateController: mock, data: oldData);
+
+      });
+      
+      test("should have method listen, which accepts map and apply listen from map correctly: key => listen filter, value => callback", () {
+        store.listen({filter: callback});
+        
+        dispatcherStream.getLogs(callsTo("where", filter)).verify(happenedOnce);
+        filteredDispatcherStream.getLogs(callsTo("listen", callback)).verify(happenedOnce);
+      });
+      
+      test("should replace filter by compare function if filter is String", () {
+        store.listen({"something to compare": callback});
+        
+        dispatcherStream.getLogs(callsTo("where", (filter) {
+          return filter("something to compare") && !filter("something else"); 
+        })).verify(happenedOnce);
+      });
+
+      test("should filter by type if event is Map and filter is String", () {
+        store.listen({"something to compare": callback});
+        
+        dispatcherStream.getLogs(callsTo("where", (filter) {
+          return filter({TYPE: "something to compare"}) && !filter({TYPE: "something else"}); 
+        })).verify(happenedOnce);
+      });
+      
+      test("should filter by attribute type if event is PersistentMap and filter is String", () {
+        store.listen({"something to compare": callback});
+        
+        dispatcherStream.getLogs(callsTo("where", (filter) {
+          return filter(persistent({TYPE: "something to compare"})) 
+              && !filter(persistent({TYPE: "something else"})); 
+        })).verify(happenedOnce);
+      });
+
+    });
+    
   });
 }
 
 class StreamControllerMock extends Mock implements StreamController {}
+class StreamMock extends Mock implements Stream {}
+class DispatcherMock extends Mock implements Dispatcher {}
